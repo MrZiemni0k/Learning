a6d5cd4dffe8f7d7bb3d696105775b0c4520dded
Ziemni0k <46859168+Ziemni0k@users.noreply.github.com>
2021-02-23 19:39:14 +0100

    Cosmetic changes

--- CurrentlyWorkingOn/Image_Convert_Viewer/Classes/StoreInfo.py
+++ CurrentlyWorkingOn/Image_Convert_Viewer/Classes/StoreInfo.py
@@ -18,7 +18,7 @@ class StoreInfo():
         ------------
         _dictionary(dict):
             Dictionary with form '.type':[int,int,int] 
-            Where [0] = Amount, [1] = Total size in MB (Round/2), [2] = Avarage size in MB (Round/2)
+            Where [0] = Amount, [1] = Total size in MB (:.3f), [2] = Avarage size in MB (:.3f)
         _path(str):
             Path with images to count.
         '''
@@ -27,42 +27,46 @@ class StoreInfo():
 
     def count_imgs(self):
         '''
-        Count images by keys in dictionary and path provided in class.
-        It counts Amount,Total Size and AVG Size. 
+        Count images by keys in dictionary and path to directory.
+        It counts Amount ,Total Size(MB)(:.3f) and AVG Size(MB)(:.3f). 
         '''
-        
-        try:
+
+        if len(self._dictionary):
             dictkeylist = self._dictionary.keys()   #Get Keys from Dictionary
 
             #For everyfile in path
             for imfile in os.listdir(self._path):
                 imfile = imfile.lower()
                 _location, _extension = os.path.splitext(imfile)    #Grab extension
+                print(imfile)
+                print(_extension)
 
-                #For each file with needed extension - We start counting
+                #Count Amount and Total Size(MB)
                 if _extension in dictkeylist:
+                    print(f'In extension -> Imfile {imfile}')
                     self._dictionary[_extension][0] += 1
                     imgsize = os.path.getsize(self._path+'/'+imfile)/1024/1024
-                    # self._dictionary[_extension][1] += round(imgsize,2) Not consistent - Will .format/str
                     self._dictionary[_extension][1] += imgsize
+                    print(f'Dict:{self._dictionary}')
             
-            #Count AVG
+            #Count AVG(MB)
             for _extension in dictkeylist:
                 if self._dictionary[_extension][0] != 0:
-                    avgsize = self._dictionary[_extension][1]/self._dictionary[_extension][0]
-                    # self._dictionary[_extension][2] = round(avgsize,2) Not consistent - Will .format/str 
+                    avgsize = self._dictionary[_extension][1]/self._dictionary[_extension][0] 
                     self._dictionary[_extension][2] = avgsize
 
-            #Round was not consistent - Change values into strings .2f
-            for _extension in dictkeylist:
-                self._dictionary[_extension][1] = f"{self._dictionary[_extension][1]:.3f}" 
-                self._dictionary[_extension][2] = f"{self._dictionary[_extension][2]:.3f}" 
-                
-        except:
-            print("Cannot count images with empty dictionary -> No keys forwarded.")
-            
-    
-            
+            #Round() was not consistent - Change values into strings .3f
+            try:
+                for _extension in dictkeylist:
+                    self._dictionary[_extension][1] = f"{self._dictionary[_extension][1]:.3f}" 
+                    self._dictionary[_extension][2] = f"{self._dictionary[_extension][2]:.3f}" 
+            except ValueError as e:
+                print(f'{e}')
+        
+        else:
+            print("StoreInfo.count_imgs():")
+            print("Dictionary is empty. Cannot count images")
+
 
 
     
